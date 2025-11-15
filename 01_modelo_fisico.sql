4e5615b54a704af7d024961f90d7eda7f1b68c0c
devNanteS <l.nantes@aluno.ifsp.edu.br>
2025-11-15 16:37:08 -0300

    Add files via upload

--- 01_modelo_fisico.sql
+++ 01_modelo_fisico.sql
@@ -16,9 +16,17 @@
 ==========================================================
 */
 
-
 create database if not exists locadora;
 use locadora;
+
+CREATE TABLE IF NOT EXISTS usuario (
+    id_usuario INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    senha VARCHAR(255) NOT NULL,
+    perfil VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id_usuario)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 CREATE TABLE IF NOT EXISTS endereco (
     id_endereco     INT NOT NULL AUTO_INCREMENT,
     logradouro      VARCHAR(255) NOT NULL,
@@ -64,66 +72,98 @@ CREATE TABLE IF NOT EXISTS cargo (
 CREATE TABLE IF NOT EXISTS funcionario (
     id_usuario      INT NOT NULL AUTO_INCREMENT, 
     nome            VARCHAR(255) NOT NULL,
+    cpf             VARCHAR(14) NOT NULL UNIQUE,
+    rg				VARCHAR(9) NOT NULL UNIQUE,
+    data_nascimento DATE NOT NULL,
     email           VARCHAR(100) NOT NULL UNIQUE,
     senha           VARCHAR(255) NOT NULL, 
     id_cargo        INT NOT NULL,
+    id_endereco		INT NOT NULL,
     PRIMARY KEY (id_usuario),
-    FOREIGN KEY (id_cargo) REFERENCES cargo(id_cargo)
+    FOREIGN KEY (id_cargo) REFERENCES cargo(id_cargo),
+    FOREIGN KEY (id_endereco) REFERENCES endereco(id_endereco)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS marca (
-    id_marca        INT NOT NULL AUTO_INCREMENT,
-    nome_marca      VARCHAR(100) NOT NULL,
+    id_marca INT NOT NULL AUTO_INCREMENT,
+    nome_marca VARCHAR(100) NOT NULL,
     PRIMARY KEY (id_marca)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS categoria_veiculo (
-    id_categoria_veiculo    INT NOT NULL AUTO_INCREMENT,
-    nome_categoria          VARCHAR(100) NOT NULL,
-    valor_diaria            DECIMAL(10, 2) NOT NULL,
+    id_categoria_veiculo INT NOT NULL AUTO_INCREMENT,
+    nome_categoria VARCHAR(100) NOT NULL,
+    valor_diaria DECIMAL(10,2) NOT NULL,
     PRIMARY KEY (id_categoria_veiculo)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS modelo (
-    id_modelo               INT NOT NULL AUTO_INCREMENT,
-    nome_modelo             VARCHAR(100) NOT NULL,
-    id_marca                INT NOT NULL,
-    id_categoria_veiculo    INT NOT NULL,
+    id_modelo INT NOT NULL AUTO_INCREMENT,
+    nome_modelo VARCHAR(100) NOT NULL,
+    id_marca INT NOT NULL,
+    id_categoria_veiculo INT NOT NULL,
     PRIMARY KEY (id_modelo),
     FOREIGN KEY (id_marca) REFERENCES marca(id_marca),
     FOREIGN KEY (id_categoria_veiculo) REFERENCES categoria_veiculo(id_categoria_veiculo)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS status_veiculo (
-    id_status_veiculo   INT NOT NULL AUTO_INCREMENT,
-    descricao_status    VARCHAR(50) NOT NULL,
+    id_status_veiculo INT NOT NULL AUTO_INCREMENT,
+    descricao_status VARCHAR(50) NOT NULL,
     PRIMARY KEY (id_status_veiculo)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS veiculo (
-    id_veiculo          INT NOT NULL AUTO_INCREMENT,
-    placa               VARCHAR(8) NOT NULL UNIQUE,
-    ano                 INT NOT NULL,
-    cor                 VARCHAR(50) NOT NULL,
-    chassi              VARCHAR(17) NOT NULL UNIQUE,
-    quilometragem       INT NOT NULL,
-    id_modelo           INT NOT NULL,
-    id_status_veiculo   INT NOT NULL,
-    PRIMARY KEY (id_veiculo),
-    FOREIGN KEY (id_modelo) REFERENCES modelo(id_modelo),
-    FOREIGN KEY (id_status_veiculo) REFERENCES status_veiculo(id_status_veiculo)
+CREATE TABLE IF NOT EXISTS combustivel (
+	id_combustivel INT NOT NULL AUTO_INCREMENT,
+    tipo_combustivel VARCHAR(100) NOT NULL,
+    PRIMARY KEY (id_combustivel)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
-CREATE TABLE IF NOT EXISTS manutencao (
-    id_manutencao       INT NOT NULL AUTO_INCREMENT,
-    data_entrada        DATE NOT NULL,
-    data_saida          DATE NULL,
-    descricao_servico   TEXT NOT NULL,
-    custo               DECIMAL(10, 2) NOT NULL,
-    id_veiculo          INT NOT NULL,
+CREATE TABLE IF NOT EXISTS seguro (
+	id_seguro INT NOT NULL AUTO_INCREMENT,
+    companhia VARCHAR(100) NOT NULL,
+    vencimento DATE NOT NULL,
+    PRIMARY KEY (id_seguro)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+CREATE TABLE IF NOT EXISTS veiculo (
+    id_veiculo INT NOT NULL AUTO_INCREMENT,
+    placa VARCHAR(8) NOT NULL UNIQUE,
+    ano INT NOT NULL,
+    cor VARCHAR(50) NOT NULL,
+    chassi VARCHAR(17) NOT NULL UNIQUE,
+    quilometragem INT NOT NULL,
+    transmissao VARCHAR(100) NOT NULL,
+    data_compra DATE NOT NULL,
+    valor_compra DECIMAL(10,2) NOT NULL,
+    data_vencimento DATE NOT NULL,
+    tanque INT NOT NULL,
+    tanque_fracao DECIMAL(5,4) NOT NULL,
+    valor_fracao DECIMAL(10,2) NOT NULL,
+    id_modelo INT NOT NULL,
+    id_status_veiculo INT NOT NULL,
+    id_combustivel INT NOT NULL,
+    id_seguro INT NULL,
+    PRIMARY KEY (id_veiculo),
+    FOREIGN KEY (id_modelo) REFERENCES modelo(id_modelo),
+    FOREIGN KEY (id_status_veiculo) REFERENCES status_veiculo(id_status_veiculo),
+    FOREIGN KEY (id_combustivel) REFERENCES combustivel(id_combustivel),
+    FOREIGN KEY (id_seguro) REFERENCES seguro(id_seguro)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+CREATE TABLE manutencao (
+    id_manutencao INT NOT NULL AUTO_INCREMENT,
+    data_entrada DATE NOT NULL,
+    data_saida DATE NULL,
+    descricao_servico TEXT NOT NULL,
+    custo DECIMAL(10,2) NOT NULL,
+    id_veiculo INT NOT NULL,
+    status ENUM('Em andamento', 'Concluída') NOT NULL DEFAULT 'Em andamento',
     PRIMARY KEY (id_manutencao),
     FOREIGN KEY (id_veiculo) REFERENCES veiculo(id_veiculo)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
 
 CREATE TABLE IF NOT EXISTS locacao (
     id_locacao                INT NOT NULL AUTO_INCREMENT,
@@ -169,4 +209,3 @@ CREATE TABLE IF NOT EXISTS opcional (
     PRIMARY KEY (id_opcional),
     FOREIGN KEY (id_locacao) REFERENCES locacao(id_locacao)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
